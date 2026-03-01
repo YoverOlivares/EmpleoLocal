d0122fad50138112ccc24b488ddcfd2abac2510f
Aldair Manosalva <amanosalvav23_2@unc.edu.pe>
2026-02-28 22:51:59 -0500

    binding en MainActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     buildFeatures {
         compose = true
     }
+    buildFeatures {
+        dataBinding = true
+    }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
