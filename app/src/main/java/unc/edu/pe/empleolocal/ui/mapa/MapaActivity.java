a42b29dcdf9ce57f5c1dda010e5a9b3335d3f09b
Aldair Manosalva <amanosalvav23_2@unc.edu.pe>
2026-03-03 20:56:35 -0500

    Proyecto Aplicada III

--- app/src/main/java/unc/edu/pe/empleolocal/ui/mapa/MapaActivity.java
+++ app/src/main/java/unc/edu/pe/empleolocal/ui/mapa/MapaActivity.java
@@ -15,6 +15,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -94,6 +95,12 @@ public class MapaActivity extends AppCompatActivity implements OnMapReadyCallbac
                 int radiusInMeters = radiusInKm * 1000;
 
                 mMap.clear(); // Limpiar mapa
+
+                // Marcador de ubicación del usuario (Punto azul)
+                mMap.addMarker(new MarkerOptions()
+                        .position(userLocation)
+                        .title("Tu ubicación")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
 
                 // Dibujar círculo actualizado
                 mMap.addCircle(new CircleOptions()
